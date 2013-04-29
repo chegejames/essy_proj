bcba8d92e4e482402507224ef2aa5e7f43f16d6c
chege <chegejames3@gmail.com>
2013-04-29 19:12:59 +0300

    push

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
   # GET /payments.json
   def index
     @member = Member.find(params[:member_id])
-    @payments = @member.payments.paginate(:page => params[:page], :per_page => 20).order
+    @payments = @member.payments.paginate(:page => params[:page], :per_page => 20).reverse
     @last_12_payments = @payments.last(12)
     @invoice = @payments.sum(:invoice)
     @amount = @payments.sum(:amount)
