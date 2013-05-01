ff46649712665877fa69c1ba580e77d40dc9b2bb
chege <chegejames3@gmail.com>
2013-05-01 08:04:16 +0300

    a

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -39,7 +39,6 @@ class PaymentsController < ApplicationController
       format.json { render json: @payment }
       format.pdf do
         render :pdf => "#{@member.first_name}_#{@member.last_name}",
-               :header => {:html => { :template => "payments/header.pdf.erb"}}
 
       end
     end
