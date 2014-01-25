3cec4eabc6124e3fa4830639bd87545d6c114341
chege <chegejames3@gmail.com>
2014-01-25 10:09:14 +0300

    zero out balances

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -43,6 +43,10 @@ class Payment < ActiveRecord::Base
     self.date.strftime("%B %Y") ==  Date.today.strftime("%B %Y")
   end
 
+   def not_paid_this_month
+     self.date.strftime("%B %Y") !=  Date.today.strftime("%B %Y")
+   end
+
   def paid_this_year?
     self.date.strftime("%Y") ==  Date.today.strftime("%Y")
   end
