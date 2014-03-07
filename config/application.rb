02d6d4c4cbd5b8ae6dbffa9b5d08bb8dceea3d24
chege <chegejames3@gmail.com>
2014-03-07 17:39:09 +0300

    scaffold

--- config/application.rb
+++ config/application.rb
@@ -75,6 +75,7 @@ module EssyProj
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+
   end
 
 end
